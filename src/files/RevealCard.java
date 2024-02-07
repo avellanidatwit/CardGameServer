ec8550dd3854a22f51729a4f1d30be6114571961
TheExplosion3 <GOwl2515@gmail.com>
2024-02-07 17:03:28 -0500

    creation of some card type files

---
+++ src/files/RevealCard.java
@@ -0,0 +1,29 @@
+package files;
+
+public class RevealCard extends Card implements CardEffects {
+
+	@Override
+	public String getEffects() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setEffects(Card e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public CardTypes getCardType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCardType(CardTypes t) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
