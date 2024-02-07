db4d36987be69fa5e069560681f970e37d4eb51a
TheExplosion3 <GOwl2515@gmail.com>
2024-02-07 08:50:02 -0500

    javadoc for turnstates

--- src/files/TurnStates.java
+++ src/files/TurnStates.java
@@ -4,12 +4,36 @@ package files;
  * @author evelyn
  */
 public enum TurnStates {
+	/**
+	 * Activates the 1st user's turn.
+	 */
 	USER1PLAY,
+	/**
+	 * Activates the 2nd user's turn.
+	 */
 	USER2PLAY,
+	/**
+	 * Get's the effects from cards played.
+	 */
 	GETEFFECTS,
+	/**
+	 * Triggers the effects from cards played.
+	 */
 	TRIGGEREFFECTS,
+	/**
+	 * Syncs the hands/deck.
+	 */
 	SYNCDECK,
+	/**
+	 * Checks for a win condition.
+	 */
 	CHECKWINCONDITIONS,
+	/**
+	 * Resets turn
+	 */
 	RESET,
+	/**
+	 * Waits for users to sync.
+	 */
 	AWAITFORSYNC
 }
